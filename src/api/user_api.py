079ad1d599ac8703878e2c7cbf45e9db5ff63543
Asenim <pasha_me1998@mail.ru>
2025-08-23 22:51:03 +0400

    added jwt to registration route

--- src/api/user_api.py
+++ src/api/user_api.py
@@ -1,19 +1,31 @@
 from fastapi import APIRouter, Depends, Request
+
 from src.rest_models import UserCreate, User
 from src.db_services import UserRepository
-
+from src.security import create_access_token, create_refresh_token
+from src.env import REFRESH_JWT_EXPIRE_MINUTES
 
 user_router = APIRouter()
 
 
 @user_router.post("/register", response_model=User)
-async def register(user: UserCreate, repo: UserRepository = Depends()):
+async def register(
+        request: Request,
+        user: UserCreate,
+        repo: UserRepository = Depends(),
+):
     created_user = await repo.create_user(username=user.username)
-    # todo: Тут будет вызов создание JWT токена
+
+    access = create_access_token(created_user.id)
+    refresh = create_refresh_token(created_user.id)
+    redis = request.app.state.redis
+    expires_in = REFRESH_JWT_EXPIRE_MINUTES * 60
+    await redis.set(
+        f"refresh:{created_user.id}:{refresh['jti']}",
+        refresh["token"], ex=expires_in)
 
     return User(
         id=created_user.id,
-        access_token="none",
-        refresh_token="none"
+        access_token=access,
+        refresh_token=refresh
     )
-
--- src/security.py
+++ src/security.py
@@ -1,6 +1,7 @@
 # auth.py
 from datetime import datetime, timedelta
 from jose import jwt
+from uuid import uuid4
 
 from src.env import (
     JWT_SECRET, JWT_ALG,
@@ -9,22 +10,30 @@ from src.env import (
 )
 
 
-def create_token(data: dict, expires_delta: timedelta):
+def __create_token(data: dict, expires_delta: timedelta):
     to_encode = data.copy()
-    expire = datetime.utcnow() + expires_delta
-    to_encode.update({"exp": expire})
-    return jwt.encode(to_encode, JWT_SECRET, algorithm=JWT_ALG)
+
+    # todo: Возможно будет лучше использовать int(datetime.now(timezone.utc))
+    iat = datetime.utcnow()
+    exp = datetime.utcnow() + expires_delta
+    jti = str(uuid4())
+    to_encode.update({"exp": exp, "iat": iat, "jti": jti})
+
+    token = jwt.encode(to_encode, JWT_SECRET, algorithm=JWT_ALG)
+
+    # todo: Возможно лучше будет сюда тоже прикрутить pydantic или хотя бы dataclass
+    return {"token": token, "jti": jti, "exp": exp, "iat": iat}
 
 
 def create_access_token(user_id: int):
-    return create_token(
+    return __create_token(
         {"sub": str(user_id), "type": "access"},
         timedelta(minutes=ACCESS_JWT_EXPIRE_MINUTES),
     )
 
 
 def create_refresh_token(user_id: int):
-    return create_token(
+    return __create_token(
         {"sub": str(user_id), "type": "refresh"},
         timedelta(minutes=REFRESH_JWT_EXPIRE_MINUTES),
     )
