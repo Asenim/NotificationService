82ac834272c5aefca552cac2e62e2979c186fda2
Asenim <pasha_me1998@mail.ru>
2025-08-25 15:22:10 +0400

    mini refactor + add exceptions

---
+++ src/db_services/user.py
@@ -0,0 +1,33 @@
+from tortoise.exceptions import DoesNotExist, IntegrityError
+from fastapi import HTTPException
+
+from src.models import User
+from src.env import AVATAR_URL
+
+
+class UserRepository:
+    @staticmethod
+    async def create_user(username: str) -> User:
+        # todo: Сделать обработку исключения когда создается пользователь с существующим username
+        try:
+            user = await User.create(
+                username=username,
+                avatar_url=AVATAR_URL,
+        )
+            return user
+        except IntegrityError:
+            raise HTTPException(
+                status_code=409,
+                detail="User with this username already exists"
+            )
+
+    @staticmethod
+    async def get_user(username: str) -> User:
+        try:
+            user = await User.get(username=username)
+            return user
+        except DoesNotExist:
+            raise HTTPException(
+                status_code=404,
+                detail="User not found"
+            )
